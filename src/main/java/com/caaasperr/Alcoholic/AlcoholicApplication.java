87007163cf3a149d60d152a3941e2fb6bfc513d6
Caaasperr <9183612@gmail.com>
2025-07-07 11:25:22 +0900

    Initial commit

---
+++ src/main/java/com/caaasperr/Alcoholic/AlcoholicApplication.java
@@ -0,0 +1,13 @@
+package com.caaasperr.Alcoholic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcoholicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caaasperr/Alcoholic/AlcoholicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caaasperr.Alcoholic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
